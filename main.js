56ed7ae28ae8bf97a7df553a038a51c6baf80e0a
Shreyas Patil <shreyaspatil367@gmail.com>
2025-11-15 10:42:15 +0530

    Assignment-3 JS

--- main.js
+++ main.js
@@ -1,80 +1,164 @@
-  
-// Question 1
-let a =10,b=3;
-console.log("Addition: ",a+b,"\nSubutraction",a-b,"\nMultiplication: ",a*b,"\nDivision: ",a/b,"\nModulus: ",a%b)
-
-// Question 2
-let x = 5;x=x+3
-console.log("X:",x,"-> += :",x += 3,"-= :",x -=3,"*= :",x *= 3,"/= :",x /= 3);
-
-// Question 3
-let count = 5;
-console.log("   --- Count -> 5 ---")
-console.log("Before:",count++,"After:",count)
-console.log("Before:",count--,"After:",count)
-
-// Question 4
-if (5 == '5') console.log("Same number different DataType notstrickly");
-if(5 === "5") console.log("Same number different DataType strickly");
-else console.log('Need to Number("5")');
-
-//Question 5
-if(a>5 && a<20 && a == 10) console.log("  -- Q5: True  ---")
-else console.log("False")
-
-//Qestion 6
-console.log(`T && F : ${true && false}`)
-console.log(`T || F : ${true || false}`)
-console.log(`!T : ${!true}`)
-
+//            --- Assignemnt - 3 ---
 // Question 7
-console.log(" --- Q7: Both true: ",(5 > 3 && 10 > 8),(5 > 3 || 10 < 8))
-
 /*
-//Question 8
-
+let age = prompt("Enter Age: ");
+if(age === null){
+    console.warn("You Entered cancel");
+}else if(parseInt(age.trim())){
+    age > 18? console.log("Eligible"):console.log("Not Eligible");
+}else{
+    console.error("Invalid Number");
+}
 */
+// Question 8
+console.log("   --- Table -> 5 ---")
+for(let i=1;i<=10;i++){
+    console.log(`5 x ${i} = ${5*i}`);
+}
+console.log("   --- End ---")
 
-console.log(bb)
-var bb = 10;
-
-
-console.log("Q1. undefined\nQ2. Error(not initialization)\nQ3.Hello\nQ4.Error(TYpeError)\nQ5.Able to use variable before declaration. (b)let\n ")
-
-
-let age = Number(prompt("Enter Age: "))
-if(age >= 18)console.log("Adult");
-else console.log("Minor");
-
-let marks = prompt('Enter your Marks: ')
-if (marks >= 90 ) console.log("A grade");
-else if (marks >= 75) console.log("“B grade”");
-else if (marks >= 50) console.log("“C grade”");
-else console.log("“Fail”");
-
-let city = "Bhopal";
-if (city == 'Bhopal') console.log("MP");
-else if (city == 'Delhi') console.log("Capital");
-else console.log("Unkown city");
-
-let score = 40;
-(score >35)? console.log("Pass"):console.log("Fail")
-
-let temperature = 27;
-(temperature > 30)?console.log("HOt"):console.log("PLeasant")
-
-let day=5;
-switch (day){
-    case 1 :console.log("Monday");break;
-    case 2 :console.log("Tuesday");break;
-    case 3 :console.log("Webnesday");break;
-    case 4 :console.log("Thusday");break;
-    case 5 :console.log("Friday");break;
-    case 6 :console.log("Saturday");break;
-    case 7 :console.log("Sunday");break;
-    default: console.log("Invalid Number")
+// Question 9
+for(let i = 1;i<16;i++){
+    if(i > 8){
+        console.log(`${i} is greater then 8`)
+    }
 }
 
-let country = 'india';
-if(age>18 && country == 'india')console.log("Eligible for Vote");
-else console.log("Not Eligible");
+// Question 10
+let userspassword = prompt("Create a password: ");
+let password = prompt("Enter Password: ");
+if(userspassword === password) console.log("Correct Password");
+else console.log("Enter wrong password.");
+
+//Question 11
+
+let i = 0,access=false;
+do{
+    let check = prompt("Enter password: ");
+    if(check === userspassword) {
+        console.log("Correct password.\n Login Successfull");
+        break;
+    }else console.log(`Wrong password ${3- i} try's left`);
+    i++;
+}while(i < 4);
+
+
+//Question 12
+
+let typed = null,count = 0;
+while(true){
+    typed = prompt("Type yes or stop");
+    if(typed === "")console.log("Please Write Something");
+    else if(typed === null) console.error("You pressed cancel");
+    else{
+        typed = typed.trim().toLowerCase();
+        if(typed === "stop") break;
+        else if(typed === "yes") count++;
+        else console.error("Please type yes or no");
+    }
+}
+
+console.log(`Yes count ${count}`);
+
+
+
+// Question 13 
+// only used this onces ok 👍
+let sum = 0;
+for(let i = 7;i<51;i+=7){
+    console.log(`${i} is divisible by 7`);
+    sum += i;
+}
+
+
+//Question 14
+sum= 0;
+for(let i=0;i<31;i++){
+    if(i%2 !== 0) sum += i;
+}
+console.log(`Sum of odd ${sum}`);
+
+//Question 15
+
+do{
+    let num = prompt("Enter a number: ");
+    if(num === "") console.warn("Entered nothing");
+    else{
+        num = parseInt(num.trim());
+        if(!num) console.error("Enter a number please");
+        else if(num % 2 === 0) break;
+    } 
+}while(true)
+
+//Question 16
+let start= prompt("Enter Starting value: ");
+let end = prompt("Enter a Ending: ");
+
+if(start === "" || end === "") console.warn("Empty values entered");
+else if(start === null || end === null) console.warn("Press cancel");
+else {
+    start = parseInt(start);end = parseInt(end);
+    if(isNaN(start) || isNaN(end)) console.log("Please enter number"); 
+    else {
+        if(start > end){
+            let temp = start;
+            start = end;
+            end = temp;
+        }
+        for(let i = start;i<end;i++){
+            console.log(i);
+        }
+    }
+}
+
+//Question 17
+count = 0;
+for(let i=1;i<20;i++){
+    if(i%2 !== 0){
+        count++;
+        if(count > 3) break;
+    }   
+}
+
+//Question 18
+count = 0;
+let asked = 0
+while(asked !== 5){
+    let num = prompt("Enter a number: ");
+    if(num === "") console.error("Enter Blank OK");
+    else if(num === null) console.warn("Pressed cancel")
+    else {
+        num = parseInt(num.trim());
+        if(num){
+            if(num > 0) count++;
+            asked++;
+        }
+        else console.error("Please enter a number...") 
+    }
+}
+console.log(`Postive number entered ${count} and negative are ${5-count}`)
+
+//Question 19 
+
+let balance = 1000,valid= false;
+asked = 0;
+while(asked <= 3){
+    let amount = prompt("Enter withdrawal amount...");
+    if(amount === null || amount === "") console.log("Nothing withdraw");
+    else{ 
+        amount = parseInt(amount.trim());
+        if(amount){
+            if(amount > balance) console.log("YE BHIK MANGYA..ITNA PAISA NAHI HE TERE PASS");
+            else{
+                balance -= amount;
+                console.log("Amount Withdrawn");
+            }
+            valid = true;
+            break;
+        }
+        else console.error("Invalid number!!!")
+    }
+    asked++;
+}
+if(valid) console.log(`Current Balance: ${balance}`);
+else console.error("Account locked!!")
